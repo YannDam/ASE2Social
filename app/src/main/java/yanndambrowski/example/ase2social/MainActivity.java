598088ad3ae3304201be401ea9b77184911ade80
YannDam <yann@stimis.de>
2021-11-16 14:33:41 +0100

    Version 0: Start

---
+++ app/src/main/java/yanndambrowski/example/ase2social/MainActivity.java
@@ -0,0 +1,14 @@
+package yanndambrowski.example.ase2social;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
